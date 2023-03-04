7fe6e139457e6f1e7c9052cdd47ad7f547893f8c
PanchoDelRancho <faparicio.fa@gmail.com>
2023-03-04 00:20:01 -0600

    mongoose population

--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose')
+const { String } = require('mongoose/lib/schema/index')
 const passportLocalMongoose = require('passport-local-mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
+  firstname: {
+    type: String,
+    default: ''
+  },
+  lastname: {
+    type: String,
+    default: ''
+  },
   username: {
     type: String,
     required: true,
